212d2e61bdcc5f5602379d9e05c29e7a2f2f2ba3
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2021-06-06 22:57:44 -0500

    final commit

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tasks',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
+  @Input()task:any = null;
   constructor() { }
 
   ngOnInit() {
